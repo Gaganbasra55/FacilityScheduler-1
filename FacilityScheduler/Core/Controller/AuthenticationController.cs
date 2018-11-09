930133d6e3c090d5ac944e94b9b2d7c9c5f91f5a
Frederico Alexandre <fred.bralvalex@gmail.com>
2018-11-08 22:31:46 -0500

    update from UI and creating DA and controller folder
    update from UI and creating DA and controller folder

---
+++ FacilityScheduler/Core/Controller/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FacilityScheduler.Core.DA;
+
+namespace FacilityScheduler.Core.Controller
+{
+    public class AuthenticationController
+    {
+        private static AuthenticationController instance;
+        private UserDA userDA;
+
+        public static AuthenticationController getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new AuthenticationController();
+            }
+            return instance;
+        }
+
+        AuthenticationController()
+        {
+            userDA = UserDA.getInstance();
+        }
+    }
+}
---
+++ FacilityScheduler/Core/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacilityScheduler.Core.Models
+{
+    public class User
+    {
+        private int Id { get; set; }
+        private string Email { get; set; }
+        private string UserName { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+
+        //Password dont need to be here
+
+        private char Type { get; set; }
+
+    }
+}
