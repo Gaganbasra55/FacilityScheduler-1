3c0ae065302c86c4ede80dff99f3e109614ce656
Frederico Alexandre <fred.bralvalex@gmail.com>
2018-11-09 22:51:46 -0500

    user session management
    user session management

--- FacilityScheduler/MasterPage.Master.designer.cs
+++ FacilityScheduler/MasterPage.Master.designer.cs
@@ -40,6 +40,33 @@ namespace FacilityScheduler {
         protected global::System.Web.UI.WebControls.Label timeAndDate;
         
         /// <summary>
+        /// LinkButtonHome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButtonHome;
+        
+        /// <summary>
+        /// LinkButtonFacility control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButtonFacility;
+        
+        /// <summary>
+        /// LinkButtonLogout control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButtonLogout;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
