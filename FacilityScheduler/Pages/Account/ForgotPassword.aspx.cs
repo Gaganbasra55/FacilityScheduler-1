9f1d6a5d3da579c5445ff3a6b9a2bd2f7f72ea1d
Shyam Panchal <shyampanchal95@gmail.com>
2018-11-22 16:29:02 -0500

    half 2 factor verification done

--- FacilityScheduler/Pages/Account/ForgotPassword.aspx.cs
+++ FacilityScheduler/Pages/Account/ForgotPassword.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
 
 namespace FacilityScheduler.Pages.Account
 {
@@ -16,7 +18,16 @@ namespace FacilityScheduler.Pages.Account
 
         protected void buttonForgotPassword_Click(object sender, EventArgs e)
         {
-            
+            string email = textboxForgotPassword.Text;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+
+            smtp.Credentials = new NetworkCredential("schedulerfacilities@gmail.com", "f123s456");
+
+            Random r = new Random(986532);
+            MailMessage mail = new MailMessage("schedulerfacilities@gmail.com", email, "verification code", (r.NextDouble() * 999999).ToString());
+            smtp.Send(mail);
         }
     }
 }
